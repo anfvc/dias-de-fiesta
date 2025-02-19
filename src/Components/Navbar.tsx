1e7f2ff923b8e1f013c715967fdcda37fa0fa8aa
andresvillay <andresvillay2@gmail.com>
2025-02-19 18:57:24 +0100

    added border shadow to navbar desk

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -12,30 +12,33 @@ const Navbar = () => {
   };
 
   return (
-    <header className="w-full sticky top-0 z-40">
+    <header className="w-full sticky shadow-gray-600 shadow-sm top-0 z-40">
       <nav className="w-full flex items-center justify-between px-4 py-3 md:px-10 max-w-[1300px] mx-auto">
-        <div className="w-full flex logo-container">
+        <div className="w-full flex items-center justify-between logo-container">
           <Link to="/">
-            <img src={logo} alt="Logo de Dias de Fiesta" className="w-40 sm:w-50" />
+            <img
+              src={logo}
+              alt="Logo de Dias de Fiesta"
+              className="w-40 sm:w-50"
+            />
           </Link>
+          <ul className="hidden gap-7 sm:flex sm:text-3xl lg:text-4xl font-semibold">
+            <li>
+              <Link to="/about">Nosotros</Link>
+            </li>
+            <li>
+              <Link to="/services">Servicios</Link>
+            </li>
+            <li>
+              <Link to="/portfolio">Portafolio</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contáctanos</Link>
+            </li>
+          </ul>
         </div>
 
         <HamburgerMenu toggleMenu={handleMenuOpenOrClose} isOpen={isOpen} />
-
-        <ul className="hidden gap-7 sm:flex sm:text-3xl font-semibold mr-4">
-          <li>
-            <Link to="/about">Nosotros</Link>
-          </li>
-          <li>
-            <Link to="/services">Servicios</Link>
-          </li>
-          <li>
-            <Link to="/portfolio">Portafolio</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contáctanos</Link>
-          </li>
-        </ul>
       </nav>
     </header>
   );
