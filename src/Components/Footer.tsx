d0b5dc128e4f458db909be91e39786c5465b046c
andresvillay <andresvillay2@gmail.com>
2025-02-28 15:42:25 +0100

    improved footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -6,24 +6,22 @@ const Footer = () => {
       className="w-full bg-[#AA9A45] relative border-black py-20 px-10 text-white "
     >
       <div className="absolute bg-black/30 inset-0"></div>
-        <div className="flex flex-col gap-4 z-10 relative">
-          <div>
-            <img
-              src={logoWhite}
-              alt="Dias de Fiesta logo white"
-              className="w-50"
-            />
-          </div>
-          <h2 className="text-4xl font-bold">
+      <div className="flex flex-col gap-4 z-10 relative">
+        <div>
+          <img
+            src={logoWhite}
+            alt="Dias de Fiesta logo white"
+            className="w-50"
+          />
+        </div>
+          <h2 className="text-4xl w-2/3 font-bold">
             Convierte tus celebraciones en experiencias inigualables
           </h2>
-          <div className="w-2/3">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-              commodi totam vel sed minima magni dolore placeat temporibus
-              consequatur tempora.
-            </p>
-          </div>
+          <p className="w-2/3">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
+            commodi totam vel sed minima magni dolore placeat temporibus
+            consequatur tempora.
+          </p>
         </div>
     </section>
   );
