7952c9281b4a819d03f8a3e9e4e93bc92a5439ad
andresvillay <andresvillay2@gmail.com>
2025-03-10 15:40:18 +0100

    fixed opacity issue on footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -11,11 +11,11 @@ const Footer = () => {
   return (
     <section
       id="footer"
-      className="w-full bg-[#AA9A45] relative border-black py-20 px-10 text-white "
+      className="w-full bg-[#AA9A45] relative border-black py-30 px-10"
     >
       <div className="absolute bg-black/30 inset-0"></div>
-      <div className="w-full flex flex-col md:flex-row gap-16 max-w-[1300px] mx-auto">
-        <div className="w-full flex flex-col gap-10 z-10 relative">
+      <div className="w-full flex flex-col gap-10 md:flex-row  md:gap-16 max-w-[1300px] mx-auto">
+        <div className="w-full flex flex-col gap-7 z-10 relative text-white">
           <div>
             <img
               src={logoWhite}
@@ -31,16 +31,16 @@ const Footer = () => {
             commodi totam vel sed minima magni dolore placeat temporibus
             consequatur tempora.
           </p>
-          <button className="w-xs p-4 bg-white text-black font-semibold rounded-full">
+          <button className="w-xs p-4 bg-white text-black font-semibold rounded-full cursor-pointer">
             Organiza tu evento
           </button>
         </div>
-        <div className="w-1/3 flex flex-col justify-center text-3xl gap-6">
+        <div className="w-1/3 flex flex-col justify-center text-3xl gap-6 relative text-white">
           <div className="flex flex-col">
             <h4>Teléfonos</h4>
             <p>{telephone}</p>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col text-white">
             <h4>Correos</h4>
             <p>{companyEmail}</p>
           </div>
@@ -50,8 +50,8 @@ const Footer = () => {
             <IoLogoWhatsapp />
           </div>
         </div>
-        <div className="w-1/3 flex flex-col justify-center">
-          <ul className="flex flex-col gap-6">
+        <div className="w-1/3 flex flex-col justify-center relative text-white">
+          <ul className="flex flex-col gap-6 text-3xl">
             <li>
               <Link to="/about" target="_self">
                 Nosotros
