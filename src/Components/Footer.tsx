ccdc114de52f0c27846211763ca8cb7c7c6000de
andresvillay <andresvillay2@gmail.com>
2025-03-21 11:54:52 +0100

    resized footer

--- src/Components/Footer.tsx
+++ src/Components/Footer.tsx
@@ -1,4 +1,4 @@
-import logoWhite from "@assets/svg/logoWhite.svg"
+import logoWhite from "@assets/svg/logoWhite.svg";
 import { IoLogoInstagram } from "react-icons/io5";
 import { IoLogoFacebook } from "react-icons/io5";
 import { IoLogoWhatsapp } from "react-icons/io5";
@@ -9,76 +9,71 @@ const Footer = () => {
   const companyEmail: string = import.meta.env.VITE_COMPANY_EMAIL;
 
   return (
-    <section
-      id="footer"
-      className="w-full bg-[#AA9A45] relative border-black py-40 px-10"
-    >
-      <div className="absolute bg-black/30 inset-0"></div>
-      <div className="w-full flex flex-col gap-10 md:flex-row  md:gap-16 max-w-[1500px] mx-auto">
-        <div className="w-full flex flex-col gap-8 z-10 relative text-white">
-          <div>
-            <img
-              src={logoWhite}
-              alt="Dias de Fiesta logo white"
-              className="w-70"
-            />
-          </div>
-          <h2 className="w-full text-5xl font-bold text-pretty">
+    <section id="footer" className="w-full bg-[#AA9A45] relative py-20 px-8">
+      <div className="absolute inset-0 bg-black/30 z-0"></div>
+      <div className="w-full flex relative justify-start max-w-[1500px] mx-auto mb-10">
+        <img src={logoWhite} alt="Dias de Fiesta logo white" className="w-70" />
+      </div>
+      <div className="w-full flex flex-col gap-8 md:flex-row md:gap-10 max-w-[1500px] mx-auto relative z-10 text-white">
+        <div className="w-full flex flex-col justify-between gap-10 z-10 relative max-w-[800px]">
+          <h2 className="text-4xl md:text-5xl font-bold text-pretty">
             Convierte tus celebraciones en experiencias inigualables
           </h2>
-          <p className="w-full text-pretty text-3xl">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tenetur
-            commodi totam vel sed minima magni dolore placeat temporibus
-            consequatur tempora.
+          <p className="text-balance text-3xl md:text-4xl">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos laborum ipsa deserunt impedit ipsam ex molestias exercitationem, cum dolor similique, earum.
           </p>
           <button className="w-xs p-4 bg-white text-black font-semibold rounded-full cursor-pointer">
             Organiza tu evento
           </button>
         </div>
-        <div className="w-1/3 flex flex-col justify-center text-3xl gap-10 relative text-white">
-          <div className="w-full flex flex-col">
+        <div className="w-full md:w-1/3 flex flex-col justify-start gap-10 text-3xl md:text-4xl mt-8 md:mt-0">
+          <h3 className="font-bold">CONTACTO</h3>
+          <div>
             <h4>Teléfonos</h4>
             <p>{telephone}</p>
           </div>
-          <div className="w-full flex flex-col text-white text-3xl">
-            <h4 className="text-balance">Correos</h4>
+          <div>
+            <h4>Correos</h4>
             <p>{companyEmail}</p>
           </div>
-          <div className="flex gap-10 text-5xl">
+          <div className="flex gap-6">
             <IoLogoInstagram />
             <IoLogoFacebook />
             <IoLogoWhatsapp />
           </div>
         </div>
-        <div className="w-1/3 flex flex-col justify-center relative text-white">
-          <ul className="flex flex-col gap-8 text-3xl">
+        <div className="w-full md:w-1/3 flex flex-col justify-start mt-8 md:mt-0">
+          <ul className="flex flex-col gap-7 text-3xl md:text-4xl">
+            <h3 className="font-bold text-4xl">PAGINAS</h3>
             <li>
-              <Link to="/about" target="_self">
-                Nosotros
-              </Link>
+              <Link to="/">Inicio</Link>
             </li>
             <li>
-              <Link to="/services" target="_self">
-                Servicios
-              </Link>
+              <Link to="/about">Nosotros</Link>
             </li>
             <li>
-              <Link to="/portfolio" target="_self">
-                Portafolio
-              </Link>
+              <Link to="/services">Servicios</Link>
             </li>
             <li>
-              <Link to="/contact" target="_self">
-                Contáctanos
-              </Link>
+              <Link to="/portfolio">Portafolio</Link>
+            </li>
+            <li>
+              <Link to="/contact">Contáctanos</Link>
             </li>
           </ul>
         </div>
       </div>
-      <div className="w-full mt-20 border-t-2 border-white max-w-[1500px] mx-auto relative md:text-4xl">
-        <div className="flex flex-col md:flex-row justify-center md:justify-between pt-10 text-white">
-          <p> &copy; {new Date().getFullYear()} Días de Fiesta</p>
-          <p>Diseñado y desarrollado con ❤️ por <span className="underline"><a href="https://linkedin.com/in/avillay " target="_blank">Andrés Villay</a></span></p>
+      <div className="w-full mt-20 border-t-2 border-white max-w-[1500px] mx-auto relative">
+        <div className="flex flex-col items-center md:flex-row md:justify-between pt-8 text-white">
+          <p>&copy; {new Date().getFullYear()} Días de Fiesta</p>
+          <p>
+            Diseñado y desarrollado con ❤️ por{" "}
+            <span className="underline">
+              <a href="https://linkedin.com/in/avillay" target="_blank">
+                Andrés Villay
+              </a>
+            </span>
+          </p>
         </div>
       </div>
     </section>
