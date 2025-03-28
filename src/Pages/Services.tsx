444e4b8db31bc41c49aa1d057afc865b0f8fd979
andresvillay <andresvillay2@gmail.com>
2025-03-28 18:21:20 +0100

    added header part on services

--- src/Pages/Services.tsx
+++ src/Pages/Services.tsx
@@ -8,10 +8,14 @@ const Services = () => {
       className="w-full pt-[84.16px] md:pt-[92.19px] max-w-[1500px] mx-auto"
       id="services"
     >
-      <div className="mt-10 px-6 xl:px-0">
-        <div className="w-full flex flex-col gap-10 bg-gold-section">
-          <div className="h-full flex justify-center items-center mt-10">
-            <img src={serviceHeadingImg} alt="service heading image" className="w-1/2" />
+      <div className="mt-10 px-6">
+        <div className="w-full gap-10 bg-gold-section rounded-4xl px-4">
+          <div className="flex">
+            <img
+              src={serviceHeadingImg}
+              alt="service heading image"
+              className="object-cover rounded-4xl"
+            />
           </div>
           <h2>Nuestros Servicios</h2>
         </div>
