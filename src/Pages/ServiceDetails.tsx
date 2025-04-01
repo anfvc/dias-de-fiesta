ab5b83e4b517214cdb9b4c7e849a0221eab07b4a
andresvillay <andresvillay2@gmail.com>
2025-04-01 16:53:01 +0200

    adding error handling if service is undefined

--- src/Pages/ServiceDetails.tsx
+++ src/Pages/ServiceDetails.tsx
@@ -5,8 +5,11 @@ const ServiceDetails = () => {
   const { id } = useParams<{ id: string }>();
 
   const service = SERVICES.find((service) => service.id === id);
+  if (!service) {
+    return (<div>Service not found</div>);
+  }
 
-  const { name, description, image, price } = service;
+  // const { name, description, image, price } = service;
 
   console.log(service);
 
@@ -21,7 +24,8 @@ const ServiceDetails = () => {
   //   currency: "COP",
   //   minimumFractionDigits: 0,
 
-  return <div>ServiceDetails</div>;
+  return (
+  <div className="pt-[84.16px] sm:pt-[92.19px] max-w-[1500px] mx-auto">ServiceDetails</div>)
 };
 
 export default ServiceDetails;
